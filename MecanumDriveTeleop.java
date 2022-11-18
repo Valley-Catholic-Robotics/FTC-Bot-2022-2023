0196bba3ce1ad2fb2776952c93dc28c5d4b5096f
v-t-x <56634281+v-t-x@users.noreply.github.com>
2022-11-17 16:53:56 -0800

    Update MecanumDriveTeleop.java

--- MecanumDriveTeleop.java
+++ MecanumDriveTeleop.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import com.qualcomm.robotcore.util.Hardware;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -9,82 +11,145 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name="MecanumDrive: Teleop", group="Mecanum")
 
 public class MecanumDriveTeleop extends LinearOpMode {
-
+    
     /* Declare OpMode members. */
-    HardwareSetUp robot=new HardwareSetUp();   // Use a Pushbot's hardware
-   
-
+    HardwareSetUp robot=new HardwareSetUp(); 
+    // Use a Pushbot's hardware
+    
     @Override
+    
     public void runOpMode() {
+        double power=1.7;
         double x1=0; //left/right
         double y1=0; //front/back
-        
+    
         double fortyFiveInRads=-Math.PI/4;
         double cosine45 = Math.cos(fortyFiveInRads);
         double sine45 = Math.sin(fortyFiveInRads);
-        
+
         double x2=0;
         double y2=0; 
+        
+        
+        double armSp=0;
+        double arm_position=0;
+        double clawPosition=robot.CLAW_HOME; // Servo position
+        final double CLAW_SPEED = 0.01; // sets rate to move serve
+        
         /* Initialize the hardware variables.
          * The init() method of the hardware class does all the work here
          */
-        double armPosition = robot.ARM HOME; // Servo's position
-        //double clawPosition = robot. CLAW HOME; // Servo safe position
-        
-        final double ARM SPEED=0 ;// sets rate to move servo
-        //final double CLAW SPEED = 0.01; // sets rate to move serve
-         
+       
         robot.init(hardwareMap);
-
+       
         // Send telemetry message to signify robot waiting;
+       
         telemetry.addData("Say", "Hello Driver");    //
         telemetry.update();
-
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-
-        // run until the end of the match (driver presses STOP)
+        // run until the end of the match (driver presses STOP
+        robot.arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        
         while (opModeIsActive()) {
-            double spin=gamepad1.right_stick_x;
+            double spin=0;
+            armSp=-gamepad2.left_stick_y;
+            arm_position=0;
+            if (gamepad1.dpad_down) {
+                power-=0.1;
+            }
+            if (gamepad1.dpad_up) {
+                power+=0.1;
+            }
             
-            if (Math.abs(spin) > 0.1) {
-                robot.FRDrive.setPower(spin);
-                robot.BRDrive.setPower(spin);
-                robot.FLDrive.setPower(-spin);
-                robot.BLDrive.setPower(-spin);
+            if (gamepad1.left_stick_x>0){
+                spin=Math.pow(gamepad1.left_stick_x, power);
+                robot.FRDrive.setPower(spin);//1
+                robot.BRDrive.setPower(spin);//1
+                robot.FLDrive.setPower(-spin);//-1
+                robot.BLDrive.setPower(-spin);//-1  
+            }
+            else if(gamepad1.left_stick_x<0){
+                spin=-Math.pow(Math.abs(gamepad1.left_stick_x), power);
+                robot.FRDrive.setPower(spin);//1
+                robot.BRDrive.setPower(spin);//1
+                robot.FLDrive.setPower(-spin);//-1
+                robot.BLDrive.setPower(-spin);//-1  
             }
             
             else {
-            x1=-gamepad1.left_stick_x;
-            y1=gamepad1.left_stick_y;
-            
-            //need to rotate 45 degree
-            y2 = y1*cosine45 + x1*sine45;
-            x2 = x1*cosine45 - y1*sine45;
-
-            // Output the safe vales to the motor drives.
-            robot.FLDrive.setPower(x2);
-            robot.BRDrive.setPower(x2);
-            robot.FRDrive.setPower(y2);
-            robot.BLDrive.setPower(y2);
+                if(gamepad1.right_stick_x<0||gamepad1.right_stick_y<0){
+                    x1=Math.pow(-gamepad1.right_stick_x, power);
+                    y1=-Math.pow(Math.abs(gamepad1.right_stick_y), power);  
+                }
+                else{
+                    x1=-Math.pow(gamepad1.right_stick_x, power);
+                    y1=Math.pow(Math.abs(gamepad1.right_stick_y), power);  
+                }
+                
+                
+                //need to rotate 45 degree
+                x2 = x1*cosine45 - y1*sine45;
+                y2 = y1*cosine45 + x1*sine45;
+                
+                
+                robot.FLDrive.setPower(x2);
+                robot.BRDrive.setPower(x2);
+                robot.FRDrive.setPower(y2);
+                robot.BLDrive.setPower(y2);
             }
-            //Raise and lower the arm
-            ARM SPEED=gamepa2.left_stick_y;
-            armPosition = ARM SPEED; // add to the servo position so it moves
+            
+            if (gamepad2.a){ // if the "a" button is pressed on the gamepad, do this next line of code
+                robot.claw.setPosition(1); 
+               
+            }
+            else if (gamepad2.y){ // if the "y" button is pressed, then do the next Line
+                robot.claw.setPosition(0); 
+               
+            }
+        
 
-            armPosition = Range. cLip(armPosition, robot.ARM MIN_RANGE,robot.ARM_MAX_RANGE); 
-// make sure the position is valid
+            
+            //Raise and lower the arm
+            
+            if (armSp>0){
+               if(arm_position>3200) {
+                  robot.arm.setPower(0); 
+                }
+                else{
+                   robot.arm.setPower(0.4);  
+                }     
+            }
+           if (armSp<0){
+               if(arm_position<0) {  
+                 
+                  robot.arm.setPower(0); 
+                }
+                else{
+                    
+                  robot.arm.setPower(-0.1);  
+                }     
+            }
+           if (armSp==0){
+                   
+                   robot.arm.setPower(0);  
+                    
+            }
+
 
             // Send telemetry message to signify robot running;
             telemetry.addData("x1","%.2f",x1);
             telemetry.addData("y1","%.2f",y1);
-	telemetry. addData("arm", "%.2f", armPosition); 
+            telemetry.addData("arm", "%.2f",armSp);
+            telemetry.addData("claw", "%.2f",clawPosition);
+            telemetry.addData("arm_position", "%.2f",arm_position);
+            telemetry.addData("spin", "%.2f",spin);
+            telemetry.addData("Robot Power", "%.2f",power);
             telemetry.update();
 
             // Pace this loop so jaw action is reasonable speed.
             sleep(50);
-            
-            
 
         }
     }
