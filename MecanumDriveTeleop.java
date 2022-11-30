e247726488fb5fa510b30f7fdf97575681d188f9
v-t-x <56634281+v-t-x@users.noreply.github.com>
2022-11-30 15:36:51 -0800

    Update MecanumDriveTeleop.java

--- MecanumDriveTeleop.java
+++ MecanumDriveTeleop.java
@@ -1,81 +1,138 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 
-public class HardwareSetUp
-{
-    /* Public OpMode members. */
-    public DcMotor FLDrive;
-    public DcMotor FRDrive;
-    public DcMotor BLDrive;
-    public DcMotor BRDrive;
+@TeleOp(name="MecanumDrive: Teleop", group="Mecanum")
 
-    public DcMotor arm;
-    public Servo claw;
+public class MecanumDriveTeleop extends LinearOpMode {
     
-    //DigitalChannel digitalTouch;
+    /* Declare OpMode members. */
+    HardwareSetUp robot=new HardwareSetUp(); 
+    // Use a Pushbot's hardware
     
-    public final static double CLAW_HOME = 0.0; // Starting position for Servo Claw
-    public final static double CLAW_MIN_RANGE = 0.0; // Smallest number value allowed for servo position
-    public final static double CLAW_MAX_RANGE = 1.0; // Largest number value allowed for servo position
-
-    /* local OpMode members. */
-    HardwareMap hwMap;
+    @Override
     
+    public void runOpMode() {
+        double power=1.5;
+        double x1=0; //left/right
+        double y1=0; //front/back
     
-    /* Constructor */
+        double fortyFiveInRads=-Math.PI/4;
+        double cosine45 = Math.cos(fortyFiveInRads);
+        double sine45 = Math.sin(fortyFiveInRads);
+
+        double x2=0;
+        double y2=0; 
+        
+        
+        double armSp=0;
+        double arm_position=0;
+        double clawPosition=robot.CLAW_HOME; // Servo position
+        final double CLAW_SPEED = 0.01; // sets rate to move serve
+        
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+       
+        robot.init(hardwareMap);
+       
+        // Send telemetry message to signify robot waiting;
+       
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        // run until the end of the match (driver presses STOP
+        robot.arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        
+        while (opModeIsActive()) {
+            double spin=0;
+            if (gamepad2.left_stick_y>0){
+                armSp=-Math.pow(gamepad2.left_stick_y, power);
+            }
+            else{
+                armSp=Math.pow(Math.abs(gamepad2.left_stick_y), power);
+            }
+            
+            arm_position=0;
+            
+            if (gamepad1.left_stick_x>0){
+                spin=Math.pow(gamepad1.left_stick_x, power);
+                robot.FRDrive.setPower(spin);//1
+                robot.BRDrive.setPower(spin);//1
+                robot.FLDrive.setPower(-spin);//-1
+                robot.BLDrive.setPower(-spin);//-1  
+            }
+            else if(gamepad1.left_stick_x<0){
+                spin=-Math.pow(Math.abs(gamepad1.left_stick_x), power);
+                robot.FRDrive.setPower(spin);//1
+                robot.BRDrive.setPower(spin);//1
+                robot.FLDrive.setPower(-spin);//-1
+                robot.BLDrive.setPower(-spin);//-1  
+            }
+            
+            else {
+                if(gamepad1.right_stick_x<0||gamepad1.right_stick_y<0){
+                    if(gamepad1.right_stick_x<0){
+                        x1=Math.pow(-gamepad1.right_stick_x, power);
+                    }
+                    if(gamepad1.right_stick_y<0){
+                        y1=-Math.pow(Math.abs(gamepad1.right_stick_y), power);  
+                    }
+                }   
+                else{
+                    x1=-Math.pow(gamepad1.right_stick_x, power);
+                    y1=Math.pow(Math.abs(gamepad1.right_stick_y), power);  
+                }
+                
+                
+                //need to rotate 45 degree
+                x2 = x1*cosine45 - y1*sine45;
+                y2 = y1*cosine45 + x1*sine45;
+                
+                
+                robot.FLDrive.setPower(x2);
+                robot.BRDrive.setPower(x2);
+                robot.FRDrive.setPower(y2);
+                robot.BLDrive.setPower(y2);
+            }
+            
+            if (gamepad2.a){ // if the "a" button is pressed on the gamepad, do this next line of code
+                robot.claw.setPosition(1); 
+               
+            }
+            else if (gamepad2.y){ // if the "y" button is pressed, then do the next Line
+                robot.claw.setPosition(0); 
+               
+            }
+        
+
+            
+            //Raise and lower the arm
+            
+           robot.arm.setPower(armSp); 
 
 
-    /* Initialize standard Hardware interfaces */
-    public void init(HardwareMap ahwMap) {
-        // Save reference to Hardware map
-        hwMap = ahwMap;
+            // Send telemetry message to signify robot running;
+            telemetry.addData("x1","%.2f",x1);
+            telemetry.addData("y1","%.2f",y1);
+            telemetry.addData("arm", "%.2f",armSp);
+            telemetry.addData("claw", "%.2f",clawPosition);
+            telemetry.addData("arm_position", "%.2f",arm_position);
+            telemetry.addData("spin", "%.2f",spin);
+            telemetry.addData("Robot Power", "%.2f",power);
+            telemetry.update();
 
-        // Define and Initialize Motors
-        FLDrive=hwMap.get(DcMotor.class, "FL");
-        FRDrive=hwMap.get(DcMotor.class, "FR");
-        BLDrive=hwMap.get(DcMotor.class, "BL");
-        BRDrive=hwMap.get(DcMotor.class, "BR");
-        
-        arm=hwMap.get(DcMotor.class, "arm");
-        claw = hwMap.servo.get("claw");// set equal to name of the servo motor in the phone
-        // setPosition actually sets the servo's position and moves it
+            // Pace this loop so jaw action is reasonable speed.
+            sleep(50);
 
-        //digitalTouch = hwMap.get(DigitalChannel.class, "sensor_digital");
-        
-        FLDrive.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        FRDrive.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
-        BLDrive.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        BRDrive.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
-        
-        arm.setDirection(DcMotor.Direction.REVERSE);
-        claw.setPosition(CLAW_HOME);
-        
-         
-        // Set all motors to zero power
-        FLDrive.setPower(0);
-        FRDrive.setPower(0);
-        BLDrive.setPower(0);
-        BRDrive.setPower(0);
-        
-        arm.setPower(0);
-
-        // Set all motors to run without encoders.
-        // May want to use RUN_USING_ENCODERS if encoders are installed.
-        FLDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        FRDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BLDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BRDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        
-        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        
-        //digitalTouch.setMode(DigitalChannel.Mode.INPUT);
-
+        }
     }
- }
-
+}
